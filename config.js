d694ce5f5756bfeaa11259223ff9f34f7a51fb8c
Alex-McAvoy <931772462@qq.com>
2024-10-05 02:22:57 +0800

    [Add] 新增配置文件

---
+++ config.js
@@ -0,0 +1,13 @@
+// 应用全局配置
+module.exports = {
+  transpileDependencies: ['uview-ui'],
+  baseUrl: 'https://github.com/Alex-McAvoy/Legends-of-the-Three-Kingdoms-Offline-Assistant',
+  appInfo: {
+    // 应用名称
+    name: "Legends-of-the-Three-Kingdoms-Offline-Assistant",
+    // 应用版本
+    version: "Alpha 1.0.0",
+  },
+}
+
+
